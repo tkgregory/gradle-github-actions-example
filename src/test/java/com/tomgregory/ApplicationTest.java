9791c23085d17d7a0c8f308d807a949fbe56ae47
Tom Gregory <t.k.gregory@gmail.com>
2021-11-18 14:15:06 +0000

    Add unit test.

---
+++ src/test/java/com/tomgregory/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.tomgregory;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    @Test
+    public void canRunApplication() {
+        Application.main(new String[]{});
+    }
+}
