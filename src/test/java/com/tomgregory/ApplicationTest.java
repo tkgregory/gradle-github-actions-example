2fd603152996b2151858af34bf62a5809686f640
Tom Gregory <t.k.gregory@gmail.com>
2021-11-18 14:18:35 +0000

    Add failing test to simulate PR workflow failure.

--- src/test/java/com/tomgregory/ApplicationTest.java
+++ src/test/java/com/tomgregory/ApplicationTest.java
@@ -2,9 +2,16 @@ package com.tomgregory;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ApplicationTest {
     @Test
     public void canRunApplication() {
         Application.main(new String[]{});
+    }
+
+    @Test
+    public void oops() {
+        fail();
     }
 }
